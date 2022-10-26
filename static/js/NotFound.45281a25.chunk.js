6dc06af5cf365ee16fdc7491306a63080258541e
maria-khabian <mariakhabian@gmail.com>
2022-10-26 11:31:43 +0200

    Updates

---
+++ static/js/NotFound.45281a25.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_pizza=self.webpackChunkreact_pizza||[]).push([[187],{4949:function(e,a,r){r.r(a);r(2791);var t=r(7766),u=r(184);a.default=function(){return(0,u.jsx)(t.vr,{})}}}]);
+//# sourceMappingURL=NotFound.45281a25.chunk.js.map
